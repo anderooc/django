9d2776cec61e1bf874c74c14b8734fcf9314711b
anderooc <andrew.chang.22@bishops.com>
2021-03-22 10:01:52 -0700

    Connected views and urls

--- assignments/urls.py
+++ assignments/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', admin.site.urls, name-'admin'),
+    path('editProfile/', views.editProfile, name='editProfile'),
+    path('viewAssignment/', views.viewAssignment, name='viewAssignment'),
+    path('makeAssignment/', views.makeAssignment, name='makeAssignment')
 ]
