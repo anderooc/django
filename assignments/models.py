e035e9ca8518afcbae0dee529c6e096cf115babe
anderooc <andrew.chang.22@bishops.com>
2021-03-22 10:01:42 -0700

    User model

--- assignments/models.py
+++ assignments/models.py
@@ -2,15 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class user(User):
+    name = models.CharField(max_length=75)
+    is_student = models.BooleanField()
+    is_teacher = models.BooleanField()
+    def __str__(self):
+        return self.name
+
 class studentProfile(models.Model):
-    name = models.CharField(max_length=75)
     grade = models.IntegerField()
-                            
-    
+
 class teacherProfile(models.Model):
-    name = models.CharField(max_length=75)
     subject = models.CharField(max_length=50)
 
 class Assignment(models.Model):
     assignmentName = models.CharField(max_length=100)
-    
