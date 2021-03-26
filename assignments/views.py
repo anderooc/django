072476d657e4dcd396c40c3bb9f820aff01ffcd3
anderooc <andrew.chang.22@bishops.com>
2021-03-26 09:17:17 -0700

    Connected to models

--- assignments/views.py
+++ assignments/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.template import loader
+from .models import Assignment
+from django.contrib.auth.models import User, Profile, StudentProfile, TeacherProfile
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    return render(request, 'assignments/home.html')
+    context = {
+        'user' : request.user
+    }
+    return render(request, 'assignments/home.html', context)
 
 def viewProfile(request):
-    return render(request, 'assignments/userProfile.html')
+    user = User.objects.get(username = usr)
+    isStudent = get_object_or_404(Profile, pk=is_student)
+    if isStudent:
+        grade = get_object_or_404(StudentProfile, pk=grade)
+    else:
+        subject = get_object_or_404(TeacherProfile, pk=subject)
+    context = {
+        'username': usr,
+        'grade': grade,
+        'subject': subject
+    }
+    return render(request, 'assignments/userProfile.html', context)
 
 def editProfile(request):
     return render(request, 'assignments/userProfile.html')
 
 def viewAssignment(request, assignmentName):
-    return HttpResponse(assignmentName)
+    name  = get_object_or_404(Assignment, pk=assignmentName)
+    description = get_object_or_404(Assignment, pk=assignmentDescription)
+    context = {
+        'name': name,
+        'description': description
+    }
+    return HttpResponse(request, assignmentName, context)
 
 def makeAssignment(request):
-    return HttpResponse("Make your assignment here")
+    return HttpResponse(request, "Make your assignment here")
