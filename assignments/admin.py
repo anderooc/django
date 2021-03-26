47eb06b41e62ce266cde37c562ea41851ce3df24
anderooc <andrew.chang.22@bishops.com>
2021-03-26 08:44:49 -0700

    Admin and models

--- assignments/admin.py
+++ assignments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Profile, StudentProfile, TeacherProfile, Assignment
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(StudentProfile)
+admin.site.register(TeacherProfile)
+admin.site.register(Assignment)
